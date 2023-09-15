40213b54669b2c3b5298ee78bef373038bce8f8d
faguilarc <116742322+faguilarc@users.noreply.github.com>
2023-09-15 01:10:48 -0400

    Add: correct prefix in lookups

--- src/tools/lookup.py
+++ src/tools/lookup.py
@@ -27,6 +27,27 @@ BUILDING_SUBDIVISION_PREFIX = ['apto', 'bloque', 'blq', 'esc', 'escalera', 'piso
                                'apto.', 'Apto.', 'apart', 'APTO', 'apt', 'apto']
 CONJUNCTION = ['y', 'e']
 
+STREET_NAME_PREFIX_CORRECT = ['calle', 'avenida',  'carretera',  'calzada',  'pasaje',  'callejon',  'callejuela',
+                      'acera',  'terraplen', 'camino']
+BETWEEN_PREFIX_CORRECT = ['entre', ]
+CORNER_CONECTOR_PREFIX_CORRECT = [ 'esquina']
+BUILDING_PREFIX_CORRECT = ['edificio', 'EDIFICIO', 'Edificio',]
+PROPERTY_PREFIX_CORRECT = [ 'número']
+DISTANCE_PREFIX_CORRECT = [ 'kilometro', 'Kilometro', 'KILOMETRO']
+DISTANCE_SPECIFICATION_PREFIX_CORRECT =['½','¼','¾','1/4','1/2','3/4']
+LOCALITY_PREFIX_CORRECT = ['pueblo', 'Pueblo', 'poblado', 'Poblado', 'caserio', 'Caserio',
+                   'reparto', 'Reparto', 'barrio',  'comunidad', 'Comunidad', 'distrito',  'Distrito', ]
+OTHER_PREFIX_CORRECT = ['batey', 'bat', 'ciudad','finca', 'Finca']
+PLACE_PREFIX_CORRECT = ['Bar','Club','Restaurante','Hotel','Centro comercial','Supermercado','Tienda minorista',
+                'Tienda mayorista','Mercado agropecuario','Bazar','Feria','Parque',
+                'Piscina','Zonas de escalada','Finca','Clínica','Hospital','Laboratorios']
+ZONE_PREFIX_CORRECT = [ 'zona',  'Zona']
+MUNICIPALITY_PREFIX_CORRECT = ['Municipio', 'municipio', 'MUNICIPIO']
+PROVINCE_PREFIX_CORRECT = [ 'Provincia', 'provicia', 'PROVINCIA', ]
+APARTMENT_PREFIX_CORRECT = [ 'apartamento', ]
+BUILDING_SUBDIVISION_PREFIX_CORRECT = ['apto', 'bloque', 'blq', 'esc', 'escalera', 'piso', 'apartamento', 'Bloque',
+                               ]
+CONJUNTION_CORRECT = ['y', 'e']
 
 
 # Listas de palabras reservadas para cada componente modelo 2:
