fix: new implementation of custom precision metric

src/neural_networks/deep_parser_model.py
@@ -176,8 +176,8 @@ class DeepParserModel(NeuralParser):
         model = keras.Model(inputs, output, name='DeepParser')
         # Optimiser
         opt = keras.optimizers.Adam(learning_rate=0.0005)
-        metrics = [tf.metrics.CategoricalAccuracy(), tf.metrics.Precision(), tf.metrics.Recall(), DeepParserModel.my_metric]
-        # metrics = [DeepParserModel.my_metric]
+        # metrics = [tf.metrics.CategoricalAccuracy(), tf.metrics.Precision(), tf.metrics.Recall(), DeepParserModel.my_metric]
+        metrics = [DeepParserModel.my_metric_v2, tf.metrics.Precision()]
         # metrics = [tf.metrics.Accuracy()]
         # Accuracy tells you how many times the ML model was correct overall.
         # Precision is how good the model is at predicting a specific category.
@@ -188,15 +188,31 @@ class DeepParserModel(NeuralParser):
         # plot_model(model, 'DeepParse_Architecture.png')
 
     @staticmethod
+    def my_metric_v2(y_true, y_pred):
+
+        result = tf.equal(tf.argmax(y_true, axis=-1), tf.argmax(y_pred, axis=-1))
+        result = tf.reduce_sum(tf.cast(result, tf.float32))
+
+        corrects_number = result
+        wrongs_number = (float(len(y_pred)) * len(y_pred[0])) - corrects_number
+
+        true_positive = corrects_number
+        false_positive = wrongs_number
+        # false_negative = wrongs_number
+        # true_negative = corrects_number * (class_number - 1) + wrongs_number * (class_number - 2)
+
+        return true_positive/(true_positive + false_positive)
+
+    @staticmethod
     def my_metric(y_true, y_pred):
+        tf.print('--.-.-.-.---.-.-.-.')
 
         class_number = len(y_pred[0][0])
         corrects_number = 0
         wrongs_number = 0
 
         # tf.print('*******************')
-        # tf.print(tf.shape(y_pred))
-
+        tf.print(tf.shape(y_pred))
         for batch_pred_index in range(len(y_pred)):
             for words_percents_index in range(len(y_pred[batch_pred_index])):
                 max_index = tf.argmax(y_pred[batch_pred_index][words_percents_index])
@@ -205,17 +221,27 @@ class DeepParserModel(NeuralParser):
                 else:
                     wrongs_number += 1
 
-        # tf.print('corrects_number: ', corrects_number)
-        # tf.print('wrongs_number: ', wrongs_number)
+        tf.print('corrects_number: ', corrects_number)
+        tf.print('wrongs_number: ', wrongs_number)
+
         true_positive = corrects_number
         false_positive = wrongs_number
         false_negative = wrongs_number
         true_negative = corrects_number*(class_number-1) + wrongs_number*(class_number-2)
 
-        value = true_positive/(true_positive - false_positive)
+        value = true_positive/(true_positive + false_positive)
+        tf.print('value: ', value)
 
+        result = tf.equal(tf.argmax(y_true, axis=-1), tf.argmax(y_pred, axis=-1))
+        result = tf.reduce_sum(tf.cast(result, tf.float32))
 
-        return tf.convert_to_tensor(value)
+        corrects_number = result
+        wrongs_number = (float(len(y_pred)) * len(y_pred[0])) - corrects_number
+
+        tf.print('corrects_number: ', corrects_number)
+        tf.print('wrongs_number: ', wrongs_number)
+
+        return value
 
     def _create_layer_vectorization(self, name, max_len, ngrams=None, split="whitespace"):
         vectorize_layer = TextVectorization(

src/tools/address_list.py
@@ -1,8 +1,18 @@
 from src.structured_direction.classified_address_one import ClassifiedAddressOne
 import tensorflow as tf
 # assert case
-y_pred = [0.6, 0, 0, 0.501, 0, 0, 0, 0, 0, 0]
-y_real = [  1, 0, 0,     1, 0, 0, 0, 0, 0, 0]
+y_pred = [[0.6, 0, 0, 0.2, 0, 0, 0.2, 0, 0, 0],
+          [0.09, 0, 0, 0.51, 0, 0.2, 0, 0.1, 0.1, 0]]
+y_real = [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+result = tf.equal(tf.argmax(y_real, axis=-1), tf.argmax(y_pred, axis=-1))
+print(result)
+print('cast: ', tf.cast(result, tf.float32))
+result = tf.reduce_sum(tf.cast(result, tf.float32))
+print(result)
+
+
+# K.mean(K.equal(K.argmax(y_true, axis=-1), K.argmax(y_pred, axis=-1)))
 
 
 # fail case
@@ -12,12 +22,18 @@ y_real = [  1, 0, 0,     1, 0, 0, 0, 0, 0, 0]
 # test case
 # y_pred = [0.0973835886, 0.0959317908, 0.0735085383, 0.0930648819, 0.0859329849, 0.0832466409, 0.577864632, 0.501, 0.0859992132, 0.0921353698]
 # y_real = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
-# 
-vp = tf.keras.metrics.TruePositives()
-vp.update_state(y_real, y_pred)
-vp = vp.result().numpy()
 
-print('vp: ', vp)
+# precision = tf.keras.metrics.Precision()
+# precision.update_state(y_real, y_pred)
+# precision = precision.result().numpy()
+#
+# print('precision: ', precision)
+
+# vp = tf.keras.metrics.TruePositives()
+# vp.update_state(y_real, y_pred)
+# vp = vp.result().numpy()
+#
+# print('vp: ', vp)
 # 
 # vn = tf.keras.metrics.truenegatives()
 # vn.update_state(y_real, y_pred)